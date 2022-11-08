a6e08364d3bbb503a79e1643d77c759876219b0e
Alioune CISSE <60492447+Alioune-Cisse@users.noreply.github.com>
2022-11-08 20:04:45 +0000

    Update preprocess2.py

--- preprocess2.py
+++ preprocess2.py
@@ -2,6 +2,12 @@ import numpy as np
 import pandas as pd
 from pulp import *
 
+"""valeurs = {'restauration': [8000.0, 20000.0],
+ 'photo vidéo': [0.0, 100000.0],
+ 'artiste': [0.0, 180000.0],
+ 'décoration': [0.0, 50000.0],
+ 'salle': [0.0, 50000.0],
+ 'gateau': [1350.0, 2500.0]}"""
 valeurs = {'restauration': [8000.0, 20000.0],
  'photo vidéo': [0.0, 100000.0],
  'artiste': [0.0, 180000.0],
@@ -15,33 +21,31 @@ def opt(budget, choix, vals=[10, 6]):
         if i not in choix:
             valeurs[i] = [0, 0]
 
-    valeurs['nombre de personnes'] = [vals[0], vals[0]]
-    valeurs['part'] = [vals[1], vals[1]]
+    # valeurs['nombre de personnes'] = [vals[0], vals[0]]
+    # valeurs['part'] = [vals[1], vals[1]]
 
     # Initialiser les variables
     # variables = [LpVariable(i, lowBound=0, cat=LpInteger) for i in valeurs.keys()]
 
     pu = LpVariable("pu", lowBound=0, cat=LpInteger)
-    n = LpVariable("n", lowBound=0, cat=LpInteger)
+    # n = LpVariable("n", lowBound=0, cat=LpInteger)
     pv = LpVariable("pv", lowBound=0, cat=LpInteger)
     art = LpVariable("art", lowBound=0, cat=LpInteger)
     deco = LpVariable("deco", lowBound=0, cat=LpInteger)
     sal = LpVariable("sal", lowBound=0, cat=LpInteger)
     prx = LpVariable("prx", lowBound=0, cat=LpInteger)
-    prt = LpVariable("prt", lowBound=0, cat=LpInteger)
+    # prt = LpVariable("prt", lowBound=0, cat=LpInteger)
 
     # Initialiser le problème
     probleme = LpProblem(name='Répartition budget', sense=LpMaximize)
     # Ajouter la fonction objectif à maximiser au problème
-    fonction_objectif = LpAffineExpression(
-        e=pu * valeurs['nombre de personnes'][0] + pv + art + deco + sal + prx * valeurs['part'][0])
+    fonction_objectif = LpAffineExpression(e=pu * vals[0] + pv + art + deco + sal + prx * vals[1])
     probleme.setObjective(fonction_objectif)
 
     # Ajouter contraintes budgéétaire
     # print(f'Variables : {variables}')
-    contrainte_budget = LpConstraint(
-        e=pu * valeurs['nombre de personnes'][0] + pv + art + deco + sal + prx * valeurs['part'][0],
-        sense=LpConstraintEQ, name='contrainte_budgétaire', rhs=budget)
+    contrainte_budget = LpConstraint(e=pu * vals[0] + pv + art + deco + sal + prx * vals[1], sense=LpConstraintEQ,
+                                     name='contrainte_budgétaire', rhs=budget)
     probleme.add(contrainte_budget)
 
     contrainte = LpConstraint(e=pu, sense=LpConstraintGE, name='great contrainte ' + 'restauration',
@@ -58,12 +62,10 @@ def opt(budget, choix, vals=[10, 6]):
                                rhs=valeurs['photo vidéo'][1])
     probleme.add(contrainte_)
 
-    contrainte = LpConstraint(e=n, sense=LpConstraintGE, name='great contrainte ' + 'nombre de personnes',
-                              rhs=valeurs['nombre de personnes'][0])
-    probleme.add(contrainte)
-    contrainte_ = LpConstraint(e=n, sense=LpConstraintLE, name='less contrainte ' + 'nombre de personnes',
-                               rhs=valeurs['nombre de personnes'][1])
-    probleme.add(contrainte_)
+    """contrainte = LpConstraint(e=n, sense=LpConstraintGE, name='great contrainte ' + 'nombre de personnes', rhs=valeurs['nombre de personnes'][0])
+    probleme.add(contrainte)            
+    contrainte_ = LpConstraint(e=n, sense=LpConstraintLE, name='less contrainte ' + 'nombre de personnes', rhs=valeurs['nombre de personnes'][1])
+    probleme.add(contrainte_)"""
 
     contrainte = LpConstraint(e=art, sense=LpConstraintGE, name='great contrainte ' + 'artiste',
                               rhs=valeurs['artiste'][0])
@@ -89,19 +91,19 @@ def opt(budget, choix, vals=[10, 6]):
     contrainte_ = LpConstraint(e=prx, sense=LpConstraintLE, name='less contrainte ' + 'gateau', rhs=valeurs['gateau'][1])
     probleme.add(contrainte_)
 
-    contrainte = LpConstraint(e=prt, sense=LpConstraintGE, name='great contrainte ' + 'part', rhs=valeurs['part'][0])
-    probleme.add(contrainte)
+    """contrainte = LpConstraint(e=prt, sense=LpConstraintGE, name='great contrainte ' + 'part', rhs=valeurs['part'][0])
+    probleme.add(contrainte)            
     contrainte_ = LpConstraint(e=prt, sense=LpConstraintLE, name='less contrainte ' + 'part', rhs=valeurs['part'][1])
-    probleme.add(contrainte_)
+    probleme.add(contrainte_)"""
     # Initialiser le solveur
     solver = PULP_CBC_CMD(timeLimit=20, msg=True)
     probleme.solve(solver=solver)
 
     repartitions = {
         "Artiste": art.value(), "Décoration": deco.value(), "Photos Vidéos": pv.value(),
-        "Restauration": {"Prix Unitaire": pu.value(), "Invités": n.value(), "Prix total": pu.value() * n.value()},
+        "Restauration": {"Prix Unitaire": pu.value(), "Invités": vals[0], "Prix total": pu.value() * vals[0]},
         "Salle": sal.value(),
-        "Gâteau": {"Prix Unitaire": prx.value(), "Parts": prt.value(), "Prix total": prx.value() * prt.value()}
+        "Gâteau": {"Prix Unitaire": prx.value(), "Parts": vals[1], "Prix total": prx.value() * vals[1]}
     }
 
     # print(probleme)
